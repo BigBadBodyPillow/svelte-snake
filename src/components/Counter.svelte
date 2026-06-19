488317a3cb462ee626768c1c745639a3a6549b5a
BigBadBodyPillow <itsshivjagath@gmail.com>
2026-06-19 17:13:16 +0200

    initial commit

---
+++ src/components/Counter.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+const {class: className = ""} = $props()
+
+  let count: number = $state(0)
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button type="button" class="counter {className}" onclick={increment}>
+  Count is {count}
+</button>
+
+<style>
+button{
+  cursor: pointer;
+  &:hover {
+    transform: scale(0.9);
+  }
+}
+</style>
