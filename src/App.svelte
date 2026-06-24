882ff484cd9b9d3371f009f647727e8950d680f2
BigBadBodyPillow <itsshivjagath@gmail.com>
2026-06-24 03:34:35 +0200

    added prettier

--- src/App.svelte
+++ src/App.svelte
@@ -3,76 +3,70 @@
   // import viteLogo from './assets/vite.svg'
   // import heroImg from './assets/hero.png'
   import Counter from './components/Counter.svelte'
-  import type {SnakePositionType} from "./types"
+  import type { SnakePositionType } from './types'
 
-
-  let snakePostion: SnakePositionType = $state([0,0])
+  let snakePostion: SnakePositionType = $state([0, 0])
 
   const dimensions: number = 8
   const cellWidth = 28 /* px */
-  const debounceDelay = 500; // ms
+  const debounceDelay = 250 // ms
 
-  let board: HTMLElement;
+  let board: HTMLElement
   let snake: HTMLElement
   let snakeXElement: HTMLParagraphElement //for debug
-  let snakeYElement: HTMLParagraphElement//for debug
-  
-  const lastMoveTime = new Map<string, number>();
-  
-  
-  
-  const canMove = (direction: string): boolean => {
-    const now = Date.now();
-    const lastTime = lastMoveTime.get(direction) ?? 0;
-    if (now - lastTime >= debounceDelay) {
-      lastMoveTime.set(direction, now);
-      return true;
-    }
-    return false;
-  }
-  
+  let snakeYElement: HTMLParagraphElement //for debug
 
-  const setBoardDimensions = (dimension:number) => {
+  const lastMoveTime = new Map<string, number>()
+
+  const canMove = (direction: string): boolean => {
+    const now = Date.now()
+    const lastTime = lastMoveTime.get(direction) ?? 0
+    if (now - lastTime >= debounceDelay) {
+      lastMoveTime.set(direction, now)
+      return true
+    }
+    return false
+  }
+
+  const setBoardDimensions = (dimension: number) => {
     board.style.gridTemplateColumns = `repeat(${dimension}, minmax(0, 1fr))`
   }
-  
+
   // when window is loa1ded
   $effect(() => {
     setBoardDimensions(dimensions)
   })
-  
-  
+
   const drawDebug = () => {
-    const newDiv = document.createElement("div")
+    const newDiv = document.createElement('div')
 
-    newDiv.classList.add("debug-menu")
-    newDiv.style.position = "absolute"
-    newDiv.style.left = "0"
-    newDiv.style.top = "0"
-    newDiv.style.width = "200px"
-    newDiv.style.padding = "12px"
-    newDiv.style.fontFamily = "Space Grotesk"
-    newDiv.style.backgroundColor = "RGBA(0,0,0,0.8)"
-    newDiv.style.color = "white"
-    
-    const positionFieldset = document.createElement("fieldset")
-    positionFieldset.style.border = ("2px solid gray")
-    positionFieldset.style.padding = "6px"
+    newDiv.classList.add('debug-menu')
+    newDiv.style.position = 'absolute'
+    newDiv.style.left = '0'
+    newDiv.style.top = '0'
+    newDiv.style.width = '200px'
+    newDiv.style.padding = '12px'
+    newDiv.style.fontFamily = 'Space Grotesk'
+    newDiv.style.backgroundColor = 'RGBA(0,0,0,0.8)'
+    newDiv.style.color = 'white'
 
-    const positionLegend = document.createElement("legend")
-    positionLegend.textContent = "snake position"
-    positionLegend.style.marginLeft = "10px"
-    
-    snakeXElement = document.createElement("p")
+    const positionFieldset = document.createElement('fieldset')
+    positionFieldset.style.border = '2px solid gray'
+    positionFieldset.style.padding = '6px'
+
+    const positionLegend = document.createElement('legend')
+    positionLegend.textContent = 'snake position'
+    positionLegend.style.marginLeft = '10px'
+
+    snakeXElement = document.createElement('p')
     snakeXElement.textContent = `x : ${snakePostion[0]}`
-    
-    snakeYElement = document.createElement("p")
+
+    snakeYElement = document.createElement('p')
     snakeYElement.textContent = `y : ${snakePostion[1]}`
-    
-    
-    const debounceDelayElement = document.createElement("p")
+
+    const debounceDelayElement = document.createElement('p')
     debounceDelayElement.textContent = `debounce : ${debounceDelay}`
-    
+
     document.body.appendChild(newDiv)
     newDiv.appendChild(positionFieldset)
     positionFieldset.appendChild(positionLegend)
@@ -83,9 +77,8 @@
 
   drawDebug()
 
-  
   //update postions in menu debug
-  $effect(() => { 
+  $effect(() => {
     if (snakeXElement && snakeYElement) {
       snakeXElement.textContent = `x : ${snakePostion[0]}`
       snakeYElement.textContent = `y : ${snakePostion[1]}`
@@ -95,7 +88,7 @@
   const calculateMove = (stepX: number, stepY: number) => {
     const max = (dimensions - 1) * cellWidth // it starts in 1 so max is 224 - 28 or 7x28 = 196
 
-    const moveX  = stepX * cellWidth
+    const moveX = stepX * cellWidth
     const moveY = stepY * cellWidth
 
     // limit movement to be between row / col 0 - 8
@@ -104,8 +97,7 @@
 
     return [newX, newY]
   }
-  
-  
+
   const move = (stepX: number, stepY: number) => {
     const [x, y] = calculateMove(stepX, stepY)
 
@@ -114,49 +106,48 @@
     snake.style.bottom = `${y}px`
   }
 
-
   const moveRight = () => {
-    if (canMove("RIGHT")) move(1, 0)
+    if (canMove('RIGHT')) move(1, 0)
   }
   const moveLeft = () => {
-    if (canMove("LEFT")) move(-1, 0)
+    if (canMove('LEFT')) move(-1, 0)
   }
   const moveUp = () => {
-    if (canMove("UP")) move(0, 1)
+    if (canMove('UP')) move(0, 1)
   }
   const moveDown = () => {
-    if (canMove("DOWN")) move(0, -1)
+    if (canMove('DOWN')) move(0, -1)
   }
-  
-  const handleMove = (event:KeyboardEvent) => {
+
+  const handleMove = (event: KeyboardEvent) => {
     switch (event.key.toUpperCase()) {
-      case "W":
+      case 'W':
         moveUp()
         break
-      case "A":
+      case 'A':
         moveLeft()
         break
-      case "S":
+      case 'S':
         moveDown()
         break
-      case "D":
+      case 'D':
         moveRight()
         break
-      default: break
+      default:
+        break
     }
   }
-
-
 </script>
 
-<svelte:window onkeypress={handleMove}/>
+<svelte:window onkeypress={handleMove} />
 <main class="flex flex-col items-center justify-center gap-8 h-screen p-2.5">
-    <div class=" flex flex-col items-center gap-2">
-      <h1>text</h1>
-      <Counter class="bg-red-500 p-2 rounded-lg text-amber-50" label="idk" />
-    </div>
+  <div class=" flex flex-col items-center gap-2">
+    <h1>text</h1>
+    <Counter class="bg-red-500 p-2 rounded-lg text-amber-50" label="idk" />
+  </div>
 
-    <div class="board grid relative" bind:this={board} draggable="false">
+  <!-- prettier-ignore -->
+  <div class="board grid relative" bind:this={board} draggable="false">
       <!-- x by x grid -->
       {#each {length: dimensions ** 2} as _, i }
         <div class="cell w-7 h-7 bg-amber-400 border border-amber-950 hover:scale-90 hover:bg-amber-500 transition-all duration-50 select-none" 
@@ -167,14 +158,4 @@
 
       <div class="snake bg-green-500 w-5 h-5 absolute m-1 bottom-0" bind:this={snake}></div>
     </div>
-  
 </main>
-
-
-
-
-<style>
-/* .snake {
-  left: 31px;
-} */
-</style>
